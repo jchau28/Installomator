50e56cfd4bef4ae160156a4093d957bda18c2ec5
Jolan Chau <jchau@wearebond.com>
2026-04-21 15:00:13 -0700

    Updated Extensis Connect to Monotype Connect - keeping same label name

--- fragments/labels/extensisconnect.sh
+++ fragments/labels/extensisconnect.sh
@@ -1,7 +1,9 @@
 extensisconnect)
-    name="Extensis Connect"
+    name="Monotype Connect"
     type="dmg"
     downloadURL="https://links.extensis.com/extensis_connect/ec_latest?language=en&platform=mac"
-    appNewVersion=$( curl -is "$downloadURL" | grep "location:" | grep -o "[[:digit:]]\+-[[:digit:]]\+-[[:digit:]]\+" | sed -e 's/-/./g' )
+    appName="Monotype Connect.app"
+    appNewVersion=$( curl -is "$downloadURL" | grep -i "location:" | grep -o "[[:digit:]]\+-[[:digit:]]\+-[[:digit:]]\+" | sed -e 's/-/./g' )
     expectedTeamID="J6MMHGD9D6"
+    blockingProcesses=( "Monotype Connect" "Extensis Connect" )
     ;;
